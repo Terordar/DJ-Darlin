de8933640c6c21762deb2a6afb5ac6020a6e43cf
Pierre Clotuche <pcl.ext@tapptic.com>
2016-03-19 11:24:17 +0100

    Menu OK

--- resources/views/Frontend/Menus/menu_contact.blade.php
+++ resources/views/Frontend/Menus/menu_contact.blade.php
@@ -1,3 +1,2 @@
+<li><a href="{!! url("/locations") !!}">Locations</a></li>
 <li><a href="{!! url("/contactez-nous") !!}">Nous contacter</a></li>
-<li><a href="{!! url("/newsletter") !!}">Newsletter</a></li>
-<li><a href="{!! url("/livre-dor") !!}">Livre d'or</a></li>
